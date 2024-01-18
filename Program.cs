ab22ede151c119fb4e67bf63d6b82784e72d72a6
JECI <53634609+JECIIIIIII@users.noreply.github.com>
2024-01-18 10:09:26 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace Variaveis
 {
-     class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            int Lain_year_release = 1998;
+            string Lain_month_release = "July";
+            float IMDB_rating = 8.1f;
+            bool All_love_Lain = true;
 
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("Meu nome é JECI!");
+            Console.WriteLine(Lain_year_release);
+            Console.WriteLine(Lain_month_release);
+            Console.WriteLine(IMDB_rating);
+            Console.WriteLine(All_love_Lain);
             
-            Console.ReadLine();
+          //Modificando variavel 
+            IMDB_rating = 10.00f;
+            Console.WriteLine(IMDB_rating);
 
+            Console.ReadLine();
         }
     }
 }
