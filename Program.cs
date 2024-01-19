1553b70a56d659d9deb12a36f91109916a9bd190
JECI <53634609+JECIIIIIII@users.noreply.github.com>
2024-01-18 21:16:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_do_usuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Confira se esta correto! Se sim, aperte enter para confirmar e encerrar o programa!");
+            Console.WriteLine(nome);
+            Console.ReadLine();
+
+
+        }
+    }
+}
